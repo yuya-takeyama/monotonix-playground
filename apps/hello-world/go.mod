f85172edbbc5ad6e96a9876555695e2d69321ffa
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-12-28 05:22:12 +0000

    Add hello-world app

---
+++ apps/hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/yuya-takeyama/monotonix-playgound/apps/hello-world
+
+go 1.23.4
---
+++ apps/hello-world/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
