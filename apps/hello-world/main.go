7a656f9afd3e5b131e4cc4ef4d48a6170d80e6d8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2025-01-02 16:05:49 +0000

    Fix

--- apps/hello-world/main.go
+++ apps/hello-world/main.go
@@ -10,7 +10,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
 
-// Update 6
+// Update 7
 func main() {
 	http.HandleFunc("/", helloHandler)
 	log.Println("Starting server on :8080")
