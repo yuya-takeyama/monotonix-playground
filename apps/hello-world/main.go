5e1d1c080a3fc9ace70597f9c21f7bf7d6c7fc3c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2025-01-04 07:02:09 +0900

    Fix (#2)
    * Fix
    
    * Use filter-jobs-by-changed-files
    
    * Fix

--- apps/hello-world/main.go
+++ apps/hello-world/main.go
@@ -10,7 +10,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
 
-// Update 17
+// Update 18
 func main() {
 	http.HandleFunc("/", helloHandler)
 	log.Println("Starting server on :8080")
